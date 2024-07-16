ecefad38bfe341613208a7702908ad2dcf910677
Yuji Hosaka <h.kyouiku58@gmail.com>
2024-07-16 18:40:39 +0700

    Switching the Active Player: Setup active player, current score, switch player when rolled 1

--- script.js
+++ script.js
@@ -15,7 +15,10 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -29,8 +32,10 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; // change later
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Switch to next player
+    activePlayer = activePlayer === 0 ? 1 : 0;
   }
 });
