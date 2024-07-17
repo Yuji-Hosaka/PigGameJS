4748063f459359e1ce1a5e51b7e18d51c94e2577
Yuji Hosaka <h.kyouiku58@gmail.com>
2024-07-17 15:45:48 +0700

    Holding Current Score: 100 pts. check, Winner decision

--- script.js
+++ script.js
@@ -57,7 +57,16 @@ btnHold.addEventListener('click', function () {
   document.getElementById(`score--${activePlayer}`).textContent =
     scores[activePlayer];
   // 2. Check if player's score is >= 100
-  // Finish the game
+
+  if (scores[activePlayer] >= 100) {
+    // Finish the game
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+  }
   // Switch to the next player
-  switchPlayer();
+  else switchPlayer();
 });
